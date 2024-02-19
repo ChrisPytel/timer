7b50dceb020f5eaec007d702b2eda46b5ef02605
ChrisPytel <chris_pytel@hotmail.com>
2024-02-19 00:33:40 -0500

    timer1.js A4 - refactored edge case handling, is able to parse floats now instead of only integers

--- timer1.js
+++ timer1.js
@@ -1,30 +1,53 @@
-//"timer1.js A3 - timerArray is now pre-emptively filtered prior for edge cases to passing into our timer function"
+//"timer1.js A4 - refactored edge case handling, is able to parse floats now instead of integers only"
+
+/* ---------------------------------------------------Edge cases handled--------------------------------------------
+
+All strings from cmd line are converted to numbers "12" => 12  or "4.6" => 4.6
+Decimals above 0 are accepted
+Integers above 0 are accepted
+Does not take in any negative numbers
+Does not take in any strings
+Does not take in NaN
+If no valid numbers are passed in via cmd line, logs a message to console
+
+
+cmd line examples:                                     order that they call:
+
+node timer1.js 32 1 4 16 2 8                           => 1 2 4 8 16 32
+node timer1.js seven 7 5.6 -4 0.4 3.1 -42 potato       => 0.4 3.1 5.6 7 
+node timer1.js 5guysBurgersNfries                      => 5             //for some reason this runs, parseFloat is funny
+node timer1.js -12 -46 george washington               => error message
+node timer1.js                                         => error message
+--------------------------------------------------------------------------------------------------------------------*/
 
 const args = process.argv;
 const timerArray = [];
 
-for (let i = 2; i < args.length; i++) {       //instead of slicing the first two elements of the args array, i just begin the loop at 2
-  let stringToInteger = parseInt(args[i]);    //converts the strings within the initial array from cmd line into integers
+for (let i = 2; i < args.length; i++) {         //instead of slicing the first two elements of the args array, i just begin the loop at 2
+  let stringToInteger = parseFloat(args[i]);    //converts the strings within the initial array from cmd line into floats
   if (typeof stringToInteger === "number" && stringToInteger >= 0){
-    stringToInteger = stringToInteger * 1000;   //multiplies them by 1000 to convert from miliseconds to seconds  
-    timerArray.push(stringToInteger);           //pushes them to our timerArray
+    stringToInteger = stringToInteger * 1000;   
+    timerArray.push(stringToInteger);  //multiplies them by 1000 to convert from miliseconds to seconds and adds to our array
   }
 }
 
-console.log(`Timers are set to go off at\n`, timerArray, `\nMiliseconds from now.`);
-
 //timer will take in an array of integers, and start a sequence of timers that go off asynchronously 
 const timer = function(newTimer) {
-  for (let index = 0; index < newTimer.length; index++) {
-    setTimeout(() => {
-      process.stdout.write(`🔔 Ding Dong 🔔\nTimer went off at ${newTimer[index] / 1000} seconds!\n`);
-      process.stdout.write('\x07');      
-      process.stdout.write('\u0007');
-      console.log('\x07');              //Not sure why
-      console.log('\u0007');            //Was not able to get any of these to trigger my system sounds.
-    }, newTimer[index]);
+  if (newTimer.length === 0){
+    console.log(`Error! No array data to create timers from`);
+  }
+  else{
+    console.log(`Timers are set to go off at\n`, timerArray, `\nMiliseconds from now.\n`);
+    for (let index = 0; index < newTimer.length; index++) {
+      setTimeout(() => {
+        console.log(`🔔 Ding Dong 🔔\nTimer went off after ${newTimer[index] / 1000} seconds!\n`);
+        process.stdout.write('\x07');      
+        process.stdout.write('\u0007');         // Not sure why but I was not able to get
+        console.log('\x07');                    // any of these to trigger my system sounds.    
+        console.log('\u0007');            
+      }, newTimer[index]);
+    }
   }
 };
 
-timer(timerArray);
-console.log(`Blocking code finished executing\n`); //kept in for testing purposes
+timer(timerArray); // Blocking code finished executing
